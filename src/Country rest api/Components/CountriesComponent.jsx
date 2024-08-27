913c0ce205ac23653d054dec4c9c3005317199c6
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-08-27 06:58:40 +0600

    conditional class

--- src/Country rest api/Components/CountriesComponent.jsx
+++ src/Country rest api/Components/CountriesComponent.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const CountriesComponent = (props) => {
   const { country, isLoading, error } = props;
   const { name, flags, capital, region } = country;
-  if (isLoading)return <div>...loading</div>;
+  const [isVisit, setIsVisit] = useState(false);
+  const handleVisit = () => {
+    setIsVisit(true);
+  };
+
+  if (isLoading) return <div>...loading</div>;
   if (error) return <div>Something Went Wrong</div>;
 
   return (
@@ -15,10 +22,15 @@ const CountriesComponent = (props) => {
       <h1>Capital : {capital}</h1>
       <h1>Region : {region}</h1>
       <button
-        class="select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-5"
+        onClick={handleVisit}
+        className={
+          isVisit
+            ? "select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-5"
+            : "select-none rounded-lg border border-w-600 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-5"
+        }
         type="button"
       >
-        visit
+        {isVisit ? "Visited" : "should Visit"}
       </button>
     </div>
   );
--- src/concept/Fetching data/Advance/PracticeComponent.jsx
+++ src/concept/Fetching data/Advance/PracticeComponent.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 const PracticeComponent = ({ users, isLoading, error }) => {
   if (isLoading) return <div>...loading</div>;
   if (error) return <div>something went wrong</div>;
@@ -6,12 +5,17 @@ const PracticeComponent = ({ users, isLoading, error }) => {
   return (
     <div className="flex justify-center flex-wrap mt-20">
       {users.length &&
-        users.slice(0,5).map((user) => {
-          return <div key={user.id} className="text-center mr-10 p-5 border rounded-xl shadow-xl relative z-0 transition-all duration-300 hover:scale-110">
-            <img src={user.avatar} alt="" className="mx-auto rounded-full"/>
-            <h1>{user.first_name}</h1>
-            <h2>{user.email}</h2>
-          </div>;
+        users.slice(0, 5).map((user) => {
+          return (
+            <div
+              key={user.id}
+              className="text-center mr-10 p-5 border rounded-xl shadow-xl relative z-0 transition-all duration-300 hover:scale-110"
+            >
+              <img src={user.avatar} alt="" className="mx-auto rounded-full" />
+              <h1>{user.first_name}</h1>
+              <h2>{user.email}</h2>
+            </div>
+          );
         })}
     </div>
   );
